58607631b571ca5fc7068949089afc515ac8e201
shingo matsuoka <shingomatsuoka@shingonoMacBook-Pro.local>
2023-08-16 14:39:13 +0900

    inertia

---
+++ resources/js/Pages/Inertia/Create.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { reactive } from 'vue';
+import { Inertia } from '@inertiajs/inertia';
+
+defineProps({
+    errors:Object
+});
+
+const form = reactive({
+    title: null,
+    content:null
+});
+
+const submitFunction = () => {
+    //第一引数のurlにPOSTメソッドによって第二引数のデータを持って送信。
+    //vueファイル内において、laravelのroutes/web.phpにおいて定義したルーティング、
+    //この場合postメソッドで/inertiaというパスにformというデータを送信する
+    //というルーティング。を実行したということになる。
+    //routeメソッドの代わり。
+    Inertia.post('/inertia', form);
+}
+
+</script>
+
+<template>
+    <form @submit.prevent="submitFunction">
+        <input type="text" name="title" v-model="form.title"><br>
+        <div v-if="errors.title">{{ errors.title }}</div>
+        <input type="text" name="content" v-model="form.content"><br>
+        <div v-if="errors.content">{{ errors.content }}</div>
+        <button>送信</button>
+    </form>
+</template>
